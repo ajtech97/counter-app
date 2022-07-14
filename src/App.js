b35798e0dfd3654ae11f67803d12d0160ef24590
Ajinkya <ajinkyalonkar972@gmail.com>
2022-07-14 14:00:39 +0530

    Counter functionality using useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
 
+  const [count, setCount] = useState(0);
+  const [countertext, setCountertext] = useState('');
+
+  const addCounter = () => {
+    setCount(prevCount =>  prevCount  + 1)
+  }
+
+  const subtractCounter = () => {
+    setCount((prevCount) => prevCount - 1)
+  }
+
+
   return(
     <main>
         <h1>Counter App</h1>
-        <input type='text' class ="counter-text" placeholder='Enter name for your counter' />
-        <div class ="counter-count">{0}</div>
+        <input 
+          type='text' 
+          class ="counter-text" 
+          placeholder='Enter name for your counter' 
+          value={countertext} 
+          onChange={event => setCountertext(event.target.value)} 
+          />
+        <div class ="counter-count">
+          {count}
+        </div>
         <div class ="button-container">
-          <button class ="button-subtract">-</button>
-          <button class ="button-add">+</button>
+          <button 
+            class ="button-subtract" 
+            onClick={subtractCounter}>
+            -
+          </button>
+          <button 
+            class ="button-add" 
+            onClick={addCounter}>
+            +
+            </button>
         </div>
     </main>
   );
