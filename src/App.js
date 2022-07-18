071cd4df8f7fd3c316bf1e4c2845d65538e3c385
Ajinkya <ajinkyalonkar972@gmail.com>
2022-07-18 15:48:35 +0530

    created storage file to define function for storing and retriving local storage and used it in app.js file

--- src/App.js
+++ src/App.js
@@ -1,74 +1,61 @@
-import React from 'react';
-import { useState } from 'react';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import { COUNTER, setLocalStorage, getLocalStorage } from "./utils/storage.js";
 
 const App = () => {
+	const [count, setCount] = useState(() => {
+		const data = getLocalStorage(COUNTER);
+		return JSON.parse(data).count || 0;
+	});
 
-  const [count, setCount] = useState(() =>{
-    const saved = localStorage.getItem("count");
-    const initvalue = JSON.parse(saved);
-    return initvalue || 0;
-  });
+	const [counterText, setCounterText] = useState(() => {
+		const data = getLocalStorage(COUNTER);
+		return JSON.parse(data).counterText || 0;
+	});
 
-  const [counterText, setCounterText] = useState(() =>{
-    const saved = localStorage.getItem("counterText");
-    const initvalue = JSON.parse(saved)
-    return initvalue || "";
-  });
+	const handleCountAddition = () => {
+		setCount(prevCount => prevCount + 1);
+	};
 
-  const handleCountAddition = () => {
-    setCount(prevCount => prevCount + 1)
-  }
+	const handleCountSubtract = () => {
+		setCount(prevCount => prevCount - 1);
+	};
 
-  const handleCountSubtract = () => {
-    setCount(prevCount => prevCount - 1)
-  }
+	const handleCounterTextChange = event => {
+		setCounterText(event.target.value);
+	};
 
-  const handleCounterTextChange = (event) => {
-    setCounterText(event.target.value)
-  }
+	const handleClickOnSave = () => {
+		setLocalStorage(COUNTER, { count, counterText });
+	};
 
-  const saveCounterNameCountLocally = () =>{
-    localStorage.setItem("count",JSON.stringify(count))
-    localStorage.setItem("counterText", JSON.stringify(counterText))
-  }
-
-
-
-  return(
-    <main>
-        <h1>Counter App</h1>
-        <input 
-          type='text' 
-          class ="counter-text" 
-          placeholder='Enter name for your counter'
-          value={counterText} 
-          onChange={handleCounterTextChange} 
-          />
-        <div class ="counter-count">
-          {count}
-        </div>
-        <div class ="button-container">
-          <button 
-            class ="button-subtract" 
-            onClick={handleCountSubtract}>
-            -
-          </button>
-          <button 
-            class ="button-add" 
-            onClick={handleCountAddition}>
-            +
-            </button>
-        </div>
-        <div class ="button-container">
-          <button 
-            class ="button-save" 
-            onClick={saveCounterNameCountLocally}>
-            Save
-          </button>
-        </div>
-    </main>
-  );
-}
+	return (
+		<main>
+			<h1>Counter App</h1>
+			<input
+				type="text"
+				class="counter-text"
+				placeholder="Enter name for your counter"
+				value={counterText}
+				onChange={handleCounterTextChange}
+			/>
+			<div class="counter-count">{count}</div>
+			<div class="button-container">
+				<button class="button-subtract" onClick={handleCountSubtract}>
+					-
+				</button>
+				<button class="button-add" onClick={handleCountAddition}>
+					+
+				</button>
+			</div>
+			<div class="button-container">
+				<button class="button-save" onClick={handleClickOnSave}>
+					Save
+				</button>
+			</div>
+		</main>
+	);
+};
 
 export default App;
